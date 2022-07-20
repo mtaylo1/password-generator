36a2a0be90a394f95c1229eeafe88475f4bf1cc6
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-07-20 19:25:06 -0400

    working on if statments for each question

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,77 @@
 // Assignment code here
-
+var generateBtn = document.querySelector("#generate");
 /*Var section*/
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var number = "0123456789";
 var specialcharacters = "~`!@#$%^&*()-_+={}[]|:;'<>,.?";
-var password;
 
 
-/*User clicks button */ 
 
-function passgenerate() {
-  var uppercaseask = confirm("Do you want upper case?")
+function generatePassword() {
 
-  if uppercaseask {
-    password += uppercase
+  var password = "";
+  var specialcharacters = "";
+  //prompt for upper case
+  var uppercaseask = confirm("Do you want upper case?");
+
+  if (uppercaseask) {
+    password += uppercase;
+  }
+  //Prompt for lower case 
+  var lowercaseask = confirm("Do you want lower case?");
+
+  if (lowercaseask) {
+    password += lowercase;
   }
 
-  var lowercaseask = confirm("Do you want lower case?")
+  //Prompt for Length
+  var passlength = prompt("How many characters? must be between 8-128");
+  if (passlenthask) {
+    password = parseInt(passlength);
 
-  if lowercaseask {
-    password += lowercase
-  }
-
-  var passlength = prompt("How many characters? must be between 8-128")
-  if passlenthask {
-    password = parseInt(passlength)
-  }
+    if (passlength < 8) {
+      alert("Password must be more than 7 characters");
+      return"";
+    }
   
+    if (passlength > 128) {
+      alert("Password must be less than 128 characters");
+    }
+  
+  }
+  //Prompt for numbers
+  var numask = confirm("Would you like numbers included?");
 
+    if (numask) {
+      password += numask;
+    }
+    // Prompt for special characters
+    var specialask = confirm("Would you like special characters?");
 
+    if (specialask) {
+      password += specialask;
+    }
 }
 
-function passl
+
 
 /*Presented with series of prompts for PW criteria*/
 
 
 /*Select Which Criteria to be used in PW*/
 
-/*Prompt for Length*/
+
 
 /*User Chooses length between 8-128 characters*/
 
-/*Prompt for character types*/
-
-/*User confirms whether or not they use LCase, Ucase, Numeric values, and/or special characters*/
 
 /*Input Validation*/
 
 /*Password Processing*/
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
