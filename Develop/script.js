8fb78a6a15b3c2075ae90cce08780da64e3e6901
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-07-19 21:43:00 -0400

    moving files

---
+++ Develop/script.js
@@ -0,0 +1,35 @@
+// Assignment code here
+
+/*User clicks button */ 
+
+/*Presented with series of prompts for PW criteria*/
+
+
+/*Select Which Criteria to be used in PW*/
+
+/*Prompt for Length*/
+
+/*User Chooses length between 8-128 characters*/
+
+/*Prompt for character types*/
+
+/*User confirms whether or not they use LCase, Ucase, Numeric values, and/or special characters*/
+
+/*Input Validation*/
+
+/*Password Processing*/
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
