2b6c0e9bfcbfb302905956c28d8caba02383fa4d
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-07-21 00:38:58 -0400

    changed structure to concat and some debugging

--- Develop/script.js
+++ Develop/script.js
@@ -1,79 +1,68 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
-/*Var section*/
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var specialcharacters = "~`!@#$%^&*()-_+={}[]|:;'<>,.?";
-
-
 
 function generatePassword() {
+/*Var section*/
+var uppercaseask = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var  lowercaseask = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var  numask = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var  specialask = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", "<", ">", "?", ",", "."];
 
-  var password = "";
-  var specialcharacters = "";
-  //prompt for upper case
-  var uppercaseask = confirm("Do you want upper case?");
+//emtpy arrays
+var result = [];
+var userarray = [];
 
-  if (uppercaseask) {
-    password += uppercase;
-  }
-  //Prompt for lower case 
-  var lowercaseask = confirm("Do you want lower case?");
+uppercaseask [1]
 
-  if (lowercaseask) {
-    password += lowercase;
-  }
-
-  //Prompt for Length
-  var passlength = prompt("How many characters? must be between 8-128");
-  if (passlenthask) {
-    password = parseInt(passlength);
-
-    if (passlength < 8) {
-      alert("Password must be more than 7 characters");
-      return"";
-    }
-  
-    if (passlength > 128) {
-      alert("Password must be less than 128 characters");
-    }
-  
-  }
-  //Prompt for numbers
-  var numask = confirm("Would you like numbers included?");
-
-    if (numask) {
-      password += numask;
-    }
-    // Prompt for special characters
-    var specialask = confirm("Would you like special characters?");
-
-    if (specialask) {
-      password += specialask;
-    }
+//Prompt for password character count
+var passlength = prompt("How many characters? must be between 8-128");
+if (passlength < 8) {
+  alert("Password must be more than 7 characters");
+  return"";
 }
 
+if (passlength > 128) {
+  alert("Password must be less than 128 characters");
+  return"";
+}
+
+//Prompts for numbers, upper case, lower case and special characters
+var number = confirm ("Would you like numbers included?");
+var uppercases = confirm ("Do you want upper case?");
+var lowercases = confirm ("Do you want lower case?");
+var specials = confirm ("Would you like special characters?");
+
+//Concat to combine results 
+if (number){
+  result = result.concat(numask);
+}
+
+if (uppercases){
+    result = result.concat(uppercaseask);
+  }
+
+if (lowercases){
+    result = result.concat(lowercaseask);
+  }
+
+if (specials){
+  result = result.concat(specialask);
+}
+
+//Logging result 
+console.log(result)
 
 
-/*Presented with series of prompts for PW criteria*/
+//Array for randomization based on results above 
+for (var i = 0; i < passlength; i++) {
+  userarray.push (result[Math.floor(Math.random() * result.length)]);
+}
 
+  return userarray.join("");
 
-/*Select Which Criteria to be used in PW*/
+}
 
-
-
-/*User Chooses length between 8-128 characters*/
-
-
-/*Input Validation*/
-
-/*Password Processing*/
-
-// Get references to the #generate element
-
-
-// Write password to the #password input
+//Function to write password 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
